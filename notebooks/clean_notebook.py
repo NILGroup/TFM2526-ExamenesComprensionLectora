a05f945ecc7fff1a6049ebcbb047fc09f7c9cb8c
P3IBOL <pablofol2003@gmail.com>
2026-05-04 09:57:51 +0200

    cleaned notebook

--- notebooks/clean_notebook.py
+++ notebooks/clean_notebook.py
@@ -3,5 +3,5 @@ with open('gemma4_fine_tuning.ipynb', 'r') as f:
     notebook = json.load(f)
 if 'widgets' in notebook.get('metadata', {}):
     del notebook['metadata']['widgets']
-with open('notebook_cleaned.ipynb', 'w') as f:
+with open('gemma4_fine_tuning_cleaned.ipynb', 'w') as f:
     json.dump(notebook, f, indent=2)   
