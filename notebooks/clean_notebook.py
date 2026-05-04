992eed8f8ec98380394542325225e8c687eeffef
P3IBOL <pablofol2003@gmail.com>
2026-05-04 09:59:54 +0200

    clean jupyter notebooks

--- notebooks/clean_notebook.py
+++ notebooks/clean_notebook.py
@@ -1,7 +1,14 @@
 import json
-with open('gemma4_fine_tuning.ipynb', 'r') as f:
+with open('gemma4_few_shot.ipynb', 'r') as f:
     notebook = json.load(f)
 if 'widgets' in notebook.get('metadata', {}):
     del notebook['metadata']['widgets']
-with open('gemma4_fine_tuning_cleaned.ipynb', 'w') as f:
+with open('gemma4_few_shot_cleaned.ipynb', 'w') as f:
+    json.dump(notebook, f, indent=2)
+
+with open('qwen35_fine_tuning.ipynb', 'r') as f:
+    notebook = json.load(f)
+if 'widgets' in notebook.get('metadata', {}):
+    del notebook['metadata']['widgets']
+with open('qwen35_fine_tuning_cleaned.ipynb', 'w') as f:
     json.dump(notebook, f, indent=2)   
